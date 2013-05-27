c317c9e798282306c7ab4c2e2f421d7f5a5cd9f4
austinrivas <austinrivas@gmail.com>
2013-05-26 18:30:01 -0700

    created standalone package

--- example/example.js
+++ example/example.js
@@ -1,6 +1,22 @@
 if (Meteor.isClient) {
     Proxino.key = "1WSzwlGhsocB-hDA0-iHUg";
     Proxino.track_errors();
+
+    Template.maskDateInput.rendered = function () {
+        $(".mask_date").mask("9999/99/99",{completed:function(){alert("You typed the following: "+this.val());}});
+    };
+
+    Template.maskPhoneInput.rendered = function () {
+        $(".mask_phone").mask("(999) 999-9999",{completed:function(){alert("You typed the following: "+this.val());}});
+    };
+
+    Template.maskSerialNumInput.rendered = function () {
+        $(".mask_serialNumber").mask("9999-9999-99",{completed:function(){alert("You typed the following: "+this.val());}});
+    };
+
+    Template.maskProductNumInput.rendered = function () {
+        $(".mask_productNumber").mask("aaa-9999-a",{completed:function(){alert("You typed the following: "+this.val());}});
+    };
 }
 
 if (Meteor.isServer) {
